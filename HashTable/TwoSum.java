246b465ef12c9c3cd8b65e42ca78deee20dbb4b7
saurabh <sgarg8678@gmail.com>
2022-04-10 17:42:38 +0530

    two sum

--- HashTable/TwoSum.java
+++ HashTable/TwoSum.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> map=new HashMap<>();
-
         for(int i=0;i<nums.length;i++){
             int difference=target-nums[i];
             if(map.containsKey(difference)){
